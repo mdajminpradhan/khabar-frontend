7846c31eb33fb8676141bc73400ab8a254c5726c
mdajminpradhan <md.ajminpradhan@outlook.com>
2021-11-05 23:41:13 +0600

    React Query Provider Add

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/sass/app.scss';
-import Routes from './Routes';
-import reportWebVitals from './reportWebVitals';
-import axios from 'axios'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/sass/app.scss";
+import Routes from "./Routes";
+import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
+import { QueryClient, QueryClientProvider } from "react-query";
+const queryClient = new QueryClient();
 
-const token = JSON.parse(localStorage.getItem('jwt'));
+const token = JSON.parse(localStorage.getItem("jwt"));
 
 axios.defaults.baseURL = process.env.REACT_APP_API;
-axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token.token}` : '';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.common["Authorization"] = token
+  ? `Bearer ${token.token}`
+  : "";
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <QueryClientProvider client={queryClient}>
+      <Routes />
+    </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
