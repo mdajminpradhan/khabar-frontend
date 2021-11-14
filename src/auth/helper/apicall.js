ca365eff2ef9c137856a96d52bf726332ba2759b
mdajminpradhan <md.ajminpradhan@outlook.com>
2021-11-14 23:21:52 +0600

    Old File Add For Not Breaking Site

---
+++ src/auth/helper/apicall.js
@@ -0,0 +1,94 @@
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
+
+// createaccount api call
+export const createAccount = (name, email, password) => {
+	return axios
+		.post('/createaccount', {
+			name: name,
+			email: email,
+			password: password
+		})
+		.then((response) => {
+			return response.data;
+		})
+		.catch((error) => {
+			return error.response.data;
+		});
+};
+
+// createaccount api call
+export const loginAccount = (email, password) => {
+	return axios
+		.post('/loginaccount', {
+			email: email,
+			password: password
+		})
+		.then((response) => {
+			return response.data;
+		})
+		.catch((error) => {
+			return error.response.data;
+		});
+};
+
+// authenticate user
+export const authenticate = (token, next) => {
+	if (typeof window !== 'undefined') {
+		localStorage.setItem('jwt', JSON.stringify(token));
+		next();
+	}
+};
+
+
+// verify token
+
+export const verifyToken = () => {
+	if(localStorage.getItem('jwt')){
+
+		try {
+			var token = JSON.parse(localStorage.getItem('jwt'));
+			return jwt.verify(token.token, process.env.REACT_APP_JWTSECRET)
+
+		} catch (error) {
+			if(error){
+				return false;
+			}
+		}
+
+	}
+}
+
+
+// checking the user is authenticated or not
+export const isAuthenticated = () => {
+	if (typeof window == 'undefined') {
+		return false;
+	}
+
+	if (localStorage.getItem('jwt')) {
+		try {
+			return JSON.parse(localStorage.getItem('jwt'));
+		} catch (error) {
+			return false;
+		}
+	} else {
+		return false;
+	}
+};
+
+
+// Log out calling function
+export const logoutAccount = (next) => {
+	if(typeof window == 'undefined'){
+		return false;
+	}
+
+	if(localStorage.getItem('jwt')){
+		localStorage.removeItem('jwt')
+	}
+	
+	axios.get('/logoutaccount');
+	
+	next();
+}
