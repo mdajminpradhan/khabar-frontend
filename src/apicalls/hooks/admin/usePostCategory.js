8c8858e3c119e2918a5bf758ab0bffaf250088b0
mdajminpradhan <md.ajminpradhan@outlook.com>
2021-11-17 21:27:23 +0600

    Post Category API Call Fn. Name Modify

--- src/apicalls/hooks/admin/usePostCategory.js
+++ src/apicalls/hooks/admin/usePostCategory.js
@@ -1,7 +1,12 @@
 import { useQuery, useQueryClient, useMutation } from "react-query";
 import cogoToast from "cogo-toast";
-import { createPostCategory } from "apicalls/services/admin";
-import { getCategories } from "apicalls/services/admin";
+import {
+  createPostCategory,
+  getPostCategories,
+  getPostCategoryById,
+  updatePostCategory,
+  deletePostCategory
+} from "apicalls/services/admin";
 
 // create post category
 export const useCreatePostCategory = () => {
@@ -18,7 +23,39 @@ export const useCreatePostCategory = () => {
 };
 
 export const useGetPostCategories = () => {
-  return useQuery("postcategories", getCategories, {
+  return useQuery("postcategories", getPostCategories, {
     refetchOnWindowFocus: false,
+  });
+};
+
+export const useGetPostCategoryById = (CategoryId) => {
+  return useQuery(["postCategoryId", CategoryId], () =>
+    getPostCategoryById(CategoryId)
+  );
+};
+
+export const useUpdatePostCategory = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation(updatePostCategory, {
+    onSuccess: () => {
+      cogoToast.success("Post category updated successfully...", {
+        position: "top-right",
+      });
+      queryClient.invalidateQueries("postcategories");
+    },
+  });
+};
+
+export const useDeletePostCategory = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation(deletePostCategory, {
+    onSuccess: () => {
+      cogoToast.success("Post category deleted successfully...", {
+        position: "top-right",
+      });
+      queryClient.invalidateQueries("postcategories");
+    },
   });
 };
